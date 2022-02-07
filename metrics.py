151ca0cac8231a1f4d798bbfaa25cd9bbd13c52b
zhbbbbbb <99086416+zhbbbbbb@users.noreply.github.com>
2022-02-06 22:54:19 -0500

    Create metrics.py
    Performance metrics functions using a list of returns as input argument.

---
+++ metrics.py
@@ -0,0 +1,36 @@
+# performance metrics
+# Performance metrics functions using a list of returns as input argument
+
+# compute the path of wealth using rets
+def path(rets):
+    rets_copy = [ret for ret in rets]
+    rets_copy[0] += 1
+    for i in range(1, len(rets_copy)):
+        rets_copy[i] = rets_copy[i-1] * (1+rets_copy[i])
+    return rets_copy
+
+# cumulative return
+def cumul_ret(rets) -> float:
+    rets = [ret + 1 for ret in rets]
+    return np.prod(rets) - 1.0
+
+# stdev considering only negative returns
+def downside_risk(rets) -> float:
+    rets = [ret for ret in rets if ret < 0.0]
+    if not returns:
+        raise Exception("No downside risk.")
+    return np.std(rets) 
+
+# calculate sharpe ratio
+def sharpe(rets, rf:float = 0.0, downside:bool = False) -> float:
+    if downside:
+        return (np.mean(rets) - rf) / downside_risk(rets)
+    return (np.mean(rets) - rf) / np.std(rets)
+
+def max_drawdown(rets):
+    rets = [np.log(ret+1.0) for ret in rets]
+    currentSum, maxDrawdown= rets[0], rets[0]
+    for i in range(1, len(rets)):
+        currentSum = min(rets[i], rets[i] + currentSum)
+        maxDrawdown = min(maxDrawdown, currentSum)
+    return np.exp(maxDrawdown) - 1.0
